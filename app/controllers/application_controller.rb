5f49b7650dab2ba35b030d9b82285eb38efa7d0c
cassianoyasumitsu <cassiano@usuarioincomum.com>
2025-03-30 21:11:05 +0900

    fix navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Allow modern browsers and mobile browsers
+  allow_browser versions: { chrome: ">= 60", firefox: ">= 60", safari: ">= 12", edge: ">= 79" }
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
