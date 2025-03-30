b78ccac78f0eb8dfa5c286ccd2412083d1b88449
cassianoyasumitsu <cassiano@usuarioincomum.com>
2025-03-30 20:44:31 +0900

    booking

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
   has_many_attached :images
 
   validates :title, :description, :price_per_night, presence: true
